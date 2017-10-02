e78241eb27a5112271d3823fcc816b0f1a2cf93a
legobrokkori <ralirulego103@gmail.com>
2017-10-02 23:14:33 +0900

    Firtst commit for serverside

---
+++ api/rest.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var mysql = require('mysql')
+
+function rest_router(router, connection, md5) {
+	var self = this;
+	self.handleRoutes(router, connection, md5);
+}
+
+
+rest_router.prototype.handleRoutes = function(router, connection, md5) {
+	router.get('/users', function(req, res){
+        //res.json({"Message" : "Hello World !"});
+        connection.query("select * from users", function(err, rows) {
+        	 if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "Success", "Users" : rows});
+            }
+        });
+	});
+}
+
+/* GET users listing. */
+// router.get('/', function(request, response) {  
+// 	router.get("/",function(req,res){
+//         res.json({"Message" : "Hello World !"});
+//     });
+// 		// connection.query("select * from users", function(err, rows, fields) {
+// 		// if (err) {
+// 		// 	console.log("error: ", err);
+// 		// 	throw err;
+// 		// }
+//   //   	response.send(['rows', rows]);
+// 	//});
+// });
+
+module.exports = rest_router;
---
+++ app.js
@@ -0,0 +1,64 @@
+'use strict';
+
+// call the packages we need
+var express    = require('express');
+var mysql      = require('mysql');
+var bodyParser = require('body-parser');
+var md5        = require('MD5');
+var fs         = require('fs');
+var yaml       = require('js-yaml');
+var config     = yaml.load(fs.readFileSync('./config/default.yaml','utf8'));
+var rest       = require('./api/rest.js');
+var app        = express();
+
+function restConnection() {
+	var self = this;
+	self.connectMysql();
+}
+
+// define mysql connection
+restConnection.prototype.connectMysql = function() {
+	var self = this;
+	var pool = mysql.createPool({
+		connectionLimit : 100,
+		host     		: config.mysql.master.host, 
+ 		user     		: config.mysql.master.user,
+ 		password 		: config.mysql.master.password,
+ 		database 		: config.mysql.master.database
+	});
+	pool.getConnection(function(err, connection){
+		if (err) {
+			self.stop(err);
+		} else {
+			self.configureExpress(connection);
+		}
+	});
+}
+
+// start server
+restConnection.prototype.configureExpress = function(connection) {
+	var self = this;
+	app.use(bodyParser.urlencoded({ extended: true}));
+	app.use(bodyParser.json());
+	var router = express.Router();
+	app.use('/api', router);
+	var rest_router = new rest(router, connection, md5);
+	self.startServer();
+}
+
+// start node server
+restConnection.prototype.startServer = function() {
+	var port = process.env.PORT || 5000;
+	app.listen(port, function() {
+		console.log('Magic happens on port ' + port);
+	});
+}
+
+// stop server
+restConnection.prototype.stop = function(err) {
+	console.log("log" + err);
+	process.exit(1);
+}
+
+new restConnection();
+
