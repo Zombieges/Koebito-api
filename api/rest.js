8ccfe85f80aa8921517eca5b17beaf9f192752ad
legobrokkori <ralirulego103@gmail.com>
2017-10-09 02:05:32 +0900

    delete extra comment

--- api/rest.js
+++ api/rest.js
@@ -1,10 +1,6 @@
 'use strict';
 
-var mysql     = require('mysql');
-// var request   = require('request');
-// var url       = require('url');
-// var url_parts = url.parse(request.url, true);
-// var query     = url_parts.query;
+var mysql = require('mysql');
 
 function rest_router(router, connection, md5) {
 	var self = this;
