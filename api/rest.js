e536c51e99f91fbeb789a2227777ef6c9c5df758
legobrokkori <ralirulego103@gmail.com>
2017-10-08 17:13:08 +0900

    add voices table

--- api/rest.js
+++ api/rest.js
@@ -8,29 +8,27 @@ function rest_router(router, connection, md5) {
 }
 
 rest_router.prototype.handleRoutes = function(router, connection, md5) {
+
+    // get users info
 	router.get('/users', function(req, res) {
         connection.query("select * from users", function(err, rows) {
         	if(err) {
-                res.json({"Error" : true, "Message" : "Error executing MySQL query","err_content" : err});
+                res.json({"Error" : true, "Message" : "Error executing MySQL query","Error Content" : err});
             } else {
                 res.json({"Error" : false, "Message" : "Success", "Users" : rows});
             }
         });
 	});
+
+    // get voices info each kind
+    router.get('/voices', function(req, res) {
+        connection.query("select v.voiceId,v.kind,ownerId,v.soundUrl,v.title,u.userId,u.userName,u.imageUrl from voices as v Inner join users as u on v.ownerId = u.userId", function(err, rows) {
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query","Error Content" : err});
+            } else {
+                res.json({"Error" : false, "Message" : "Success", "Voices" : rows});
+            }
+        });
+    });
 }
-
-/* GET users listing. */
-// router.get('/', function(request, response) {  
-// 	router.get("/",function(req,res){
-//         res.json({"Message" : "Hello World !"});
-//     });
-// 		// connection.query("select * from users", function(err, rows, fields) {
-// 		// if (err) {
-// 		// 	console.log("error: ", err);
-// 		// 	throw err;
-// 		// }
-//   //   	response.send(['rows', rows]);
-// 	//});
-// });
-
 module.exports = rest_router;
