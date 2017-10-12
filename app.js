4b89ffc67a5238568fa123f096a7d1e8d2b9644b
legobrokkori <ralirulego103@gmail.com>
2017-10-13 00:22:23 +0900

    fix typo

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ restConnection.prototype.connectMysql = function() {
 		connectionLimit : 100,
 		host     		: process.env.HOST,
  		user     		: process.env.USER,
- 		password 		: process.env.password,
+ 		password 		: process.env.PASSWORD,
  		database 		: process.env.DATABASE
 	});
 	pool.getConnection(function(err, connection){
